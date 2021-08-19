219f1e59c922d2ce6250a21e1ffb5537f60a533e
Davyjones <ekenedavidson15@gmail.com>
2021-08-19 16:34:04 +0100

    push now

--- src/main/java/com/sabi/framework/FrameworkApplication.java
+++ src/main/java/com/sabi/framework/FrameworkApplication.java
@@ -2,7 +2,11 @@ package com.sabi.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.sabi")
+@EntityScan(basePackages = {"com.sabi.framework.models"})
 @SpringBootApplication
 public class FrameworkApplication {
 

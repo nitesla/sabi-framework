24b7468ad38906976b7667f6f551b38801515356
ekedon <ekene.a@spinel.consulting>
2022-06-01 13:29:07 +0100

    update

--- src/main/java/com/sabi/framework/service/UserService.java
+++ src/main/java/com/sabi/framework/service/UserService.java
@@ -615,7 +615,7 @@ public class UserService {
         String currentDate = df.format(calobj.getTime());
         String regDate = user.getResetTokenExpirationDate();
         String result = String.valueOf(currentDate.compareTo(regDate));
-        if(result.equals("1")){
+        if(!result.startsWith("-")){
             throw new BadRequestException(CustomResponseCode.BAD_REQUEST, " OTP invalid/expired");
         }
         String pin = Encryptions.generateSha256(request.getTransactionPin());

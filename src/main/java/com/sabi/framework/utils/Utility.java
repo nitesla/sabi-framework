fa6856ccde847f55863331588b69a49b07eb0c3d
muyiwa_dev <muyiwa.a@spinel.consulting>
2022-07-02 12:15:33 +0900

    latest update.

--- src/main/java/com/sabi/framework/utils/Utility.java
+++ src/main/java/com/sabi/framework/utils/Utility.java
@@ -242,12 +242,12 @@ public class Utility {
     }
 
 
-    public static String expirationForSupplyRequest() {
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(timestamp.getTime());
-        cal.setTimeInMillis(timestamp.getTime());
-        cal.add(Calendar.MINUTE, 15);
-        return String.valueOf(new Timestamp(cal.getTime().getTime()));
-    }
+//    public static String expirationForSupplyRequest() {
+//        Timestamp timestamp = new Timestamp(new Date().getTime());
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTimeInMillis(timestamp.getTime());
+//        cal.setTimeInMillis(timestamp.getTime());
+//        cal.add(Calendar.MINUTE, 15);
+//        return String.valueOf(new Timestamp(cal.getTime().getTime()));
+//    }
 }
